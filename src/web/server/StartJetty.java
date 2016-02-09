893508d09d5b3fbae37426a7e31f798606afa7c6
Levon Gevorgyan <levon.programmer@gmail.com>
2016-02-10 00:45:55 +0400

    ws: Web socket successfully configured and tested
    #added new libs

---
+++ src/web/server/StartJetty.java
@@ -0,0 +1,70 @@
+package web.server;
+import java.io.File;
+import java.lang.management.ManagementFactory;
+
+import org.eclipse.jetty.jmx.MBeanContainer;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.webapp.WebAppContext;
+import org.eclipse.jetty.websocket.server.WebSocketHandler;
+import org.eclipse.jetty.websocket.servlet.WebSocketServletFactory;
+import web.websocket.MyWebSocketHandler;
+
+/**
+ * Created by Levon on 2/9/2016, 9:12 PM
+ */
+public class StartJetty {
+    public static void main( String[] args ) throws Exception
+    {
+        //Web Server
+        Server server = new Server();
+        // HTTP connector
+        ServerConnector http = new ServerConnector(server);
+        http.setHost("0.0.0.0");
+        http.setPort(8080);
+        server.addConnector(http);
+
+        //Web Socket
+        Server socket = new Server();
+        // WS connector
+        ServerConnector ws = new ServerConnector(socket);
+        ws.setHost("0.0.0.0");
+        ws.setPort(1337);
+        socket.addConnector(ws);
+
+
+        // Setup JMX
+        MBeanContainer mbContainer = new MBeanContainer(
+                ManagementFactory.getPlatformMBeanServer());
+        server.addBean(mbContainer);
+
+        //set up context path and WEB-INF directory
+        WebAppContext webapp = new WebAppContext();
+        webapp.setContextPath("/");
+        File warFile = new File(
+                "out/artifacts/chess");
+        webapp.setWar(warFile.getAbsolutePath());
+
+        //add Web App to server
+        server.setHandler(webapp);
+
+        //create ws handler
+        WebSocketHandler wsHandler = new WebSocketHandler() {
+            @Override
+            public void configure(WebSocketServletFactory factory) {
+                factory.register(MyWebSocketHandler.class);
+            }
+        };
+        //add ws handler to socket
+        socket.setHandler(wsHandler);
+
+        // Start server,socket
+        server.start();
+        socket.start();
+
+        //Join
+        server.join();
+        socket.join();
+
+    }
+}
---
+++ src/web/websocket/WebSocketTest.java
@@ -0,0 +1,24 @@
+package web.websocket;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.websocket.server.WebSocketHandler;
+import org.eclipse.jetty.websocket.servlet.WebSocketServletFactory;
+
+/**
+ * Created by Levon on 2/9/2016, 8:49 PM
+ */
+public class WebSocketTest {
+
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(8090);
+        WebSocketHandler wsHandler = new WebSocketHandler() {
+            @Override
+            public void configure(WebSocketServletFactory factory) {
+                factory.register(MyWebSocketHandler.class);
+            }
+        };
+        server.setHandler(wsHandler);
+        server.start();
+        server.join();
+    }
+}
